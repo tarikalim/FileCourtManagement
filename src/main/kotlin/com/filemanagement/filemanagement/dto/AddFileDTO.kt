617a0af545b453bb880b1ac9b7bb2eb0e6e2cb32
Tevfik Tarık Alim <tarikalim1899@gmail.com>
2024-06-12 13:35:42 +0300

    validation for file dto and additional converters

--- src/main/kotlin/com/filemanagement/filemanagement/dto/AddFileDTO.kt
+++ src/main/kotlin/com/filemanagement/filemanagement/dto/AddFileDTO.kt
@@ -1,5 +1,13 @@
 package com.filemanagement.filemanagement.dto
 
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Pattern
+
 data class AddFileDTO(
-    val filename: String,
+    @field:NotBlank
+    @field:Pattern(
+        regexp = "\\d{4}/\\d+",
+        message = "Filename must be in the format YYYY-N, where YYYY is the year and N is a number"
     )
+    val filename: String,
+)
--- src/main/kotlin/com/filemanagement/filemanagement/dto/FileMapper.kt
+++ src/main/kotlin/com/filemanagement/filemanagement/dto/FileMapper.kt
@@ -1,7 +1,5 @@
 package com.filemanagement.filemanagement.dto
 
-import com.filemanagement.filemanagement.dto.UserFileDTO
-import com.filemanagement.filemanagement.dto.AddFileDTO
 import com.filemanagement.filemanagement.model.File
 import com.filemanagement.filemanagement.model.User
 import org.springframework.stereotype.Component
@@ -18,11 +16,34 @@ class FileMapper {
         )
     }
 
+    fun toUserFileDTO(file: File): UserFileDTO {
+        return UserFileDTO(
+            id = file.id,
+            filename = file.filename,
+            assignedUser = file.user.username
+        )
+    }
+
     // DTO to Entity
     fun fromDTO(dto: AddFileDTO, user: User): File {
         return File(
             filename = dto.filename,
             user = user
+        )
+    }
+
+    // AddFileDTO to File entity for createFile method
+    fun fromAddDTO(dto: AddFileDTO, user: User): File {
+        return File(
+            filename = dto.filename,
+            user = user
+        )
+    }
+
+    // File entity to AddFileDTO for createFile method response
+    fun toAddDTO(file: File): AddFileDTO {
+        return AddFileDTO(
+            filename = file.filename
         )
     }
 }
