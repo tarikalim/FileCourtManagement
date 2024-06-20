d2f32cf9b5d50ddebcd9e7794f79f62204bc7605
Tevfik Tarık Alim <tarikalim1899@gmail.com>
2024-06-20 21:13:20 +0300

    use env variables for application.properties. dockerized all application. ( for mysql and backend)

--- Backend/src/main/kotlin/com/filemanagement/filemanagement/FilemanagementApplication.kt
+++ Backend/src/main/kotlin/com/filemanagement/filemanagement/FilemanagementApplication.kt
@@ -1,5 +1,6 @@
 package com.filemanagement.filemanagement
 
+import io.github.cdimascio.dotenv.Dotenv
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableScheduling
@@ -9,5 +10,11 @@ import org.springframework.scheduling.annotation.EnableScheduling
 class FilemanagementApplication
 
 fun main(args: Array<String>) {
-	runApplication<FilemanagementApplication>(*args)
+    val dotenv = Dotenv.load()
+
+    dotenv.entries().forEach { entry ->
+        System.setProperty(entry.key, entry.value)
+    }
+
+    runApplication<FilemanagementApplication>(*args)
 }
