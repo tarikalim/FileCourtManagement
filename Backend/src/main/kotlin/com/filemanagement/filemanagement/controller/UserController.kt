05aba5fe73f7b6c22a6009e062c53d0f8a9e28e3
Tevfik Tarık Alim <tarikalim1899@gmail.com>
2024-06-23 01:27:30 +0300

    authentication principle implementation for current user's infos and files endpoints. And also web configuration for frontend.

--- Backend/src/main/kotlin/com/filemanagement/filemanagement/controller/UserController.kt
+++ Backend/src/main/kotlin/com/filemanagement/filemanagement/controller/UserController.kt
@@ -3,18 +3,20 @@ package com.filemanagement.filemanagement.controller
 import com.filemanagement.filemanagement.dto.CreateUserDTO
 import com.filemanagement.filemanagement.dto.FileDTO
 import com.filemanagement.filemanagement.dto.UserDTO
+import com.filemanagement.filemanagement.security.CustomUserDetails
 import com.filemanagement.filemanagement.service.FileService
 import com.filemanagement.filemanagement.service.UserService
 import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/users")
 class UserController(
     private val userService: UserService,
-    private val fileService: FileService
+    private val fileService: FileService,
 ) {
 
     @GetMapping
@@ -26,11 +28,25 @@ class UserController(
         val user = userService.getUserById(id)
         return ResponseEntity(user, HttpStatus.OK)
     }
+
+    @GetMapping("/me")
+    fun getCurrentUser(@AuthenticationPrincipal userDetails: CustomUserDetails): ResponseEntity<UserDTO> {
+        val user = userService.getUserById(userDetails.id)
+        return ResponseEntity(user, HttpStatus.OK)
+    }
+
     @GetMapping("/{id}/files")
     fun getFilesByUserId(@PathVariable id: Long): ResponseEntity<List<FileDTO>> {
         val files = fileService.getFilesByUserId(id)
         return ResponseEntity(files, HttpStatus.OK)
     }
+
+    @GetMapping("/me/files")
+    fun getFilesByCurrentUser(@AuthenticationPrincipal userDetails: CustomUserDetails): ResponseEntity<List<FileDTO>> {
+        val files = fileService.getFilesByUserId(userDetails.id)
+        return ResponseEntity(files, HttpStatus.OK)
+    }
+
 
     @PostMapping
     fun createUser(@Valid @RequestBody createUserDTO: CreateUserDTO): ResponseEntity<UserDTO> {
