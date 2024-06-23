3a65fd774484897e6c7e9807ac31ac8ed119c656
Tevfik Tarık Alim <tarikalim1899@gmail.com>
2024-06-23 13:43:26 +0300

    remove autowired

--- Backend/src/main/kotlin/com/filemanagement/filemanagement/controller/AuthController.kt
+++ Backend/src/main/kotlin/com/filemanagement/filemanagement/controller/AuthController.kt
@@ -4,7 +4,6 @@ import com.filemanagement.filemanagement.dto.AuthDTO
 import com.filemanagement.filemanagement.security.CustomUserDetails
 import com.filemanagement.filemanagement.service.JwtService
 import com.filemanagement.filemanagement.service.UserService
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
@@ -13,9 +12,9 @@ import org.springframework.web.bind.annotation.*
 @RestController
 @RequestMapping("/auth")
 class AuthenticationController(
-    @Autowired private val authenticationManager: AuthenticationManager,
-    @Autowired private val userService: UserService,
-    @Autowired private val jwtService: JwtService
+    private val authenticationManager: AuthenticationManager,
+    private val userService: UserService,
+    private val jwtService: JwtService
 ) {
 
     @PostMapping("/login")
