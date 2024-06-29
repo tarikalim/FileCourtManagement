e49f88f2920ee4b00084bbd34bf8bf7b4d861fcb
Tevfik Tarık Alim <tarikalim1899@gmail.com>
2024-06-29 11:59:16 +0300

    use spring config.import feature to manage secret variables instead of using 3. party dotenv library

--- Backend/src/main/kotlin/com/filemanagement/filemanagement/FilemanagementApplication.kt
+++ Backend/src/main/kotlin/com/filemanagement/filemanagement/FilemanagementApplication.kt
@@ -1,6 +1,5 @@
 package com.filemanagement.filemanagement
 
-import io.github.cdimascio.dotenv.Dotenv
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableScheduling
@@ -10,11 +9,7 @@ import org.springframework.scheduling.annotation.EnableScheduling
 class FilemanagementApplication
 
 fun main(args: Array<String>) {
-    val dotenv = Dotenv.load()
 
-    dotenv.entries().forEach { entry ->
-        System.setProperty(entry.key, entry.value)
-    }
 
     runApplication<FilemanagementApplication>(*args)
 }
